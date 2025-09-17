2309cb396b3a2708925c13f5dc257bb38de48cfc
Daniel Stickel <71678898+daniel-stickel@users.noreply.github.com>
2025-09-16 21:56:23 -0300

    alteração no banco

---
+++ Banco/Locadora/Select, Update e Delete.sql
@@ -0,0 +1,35 @@
+use minhalocadora;
+
+select * from carro;
+
+
+
+select * from categoria; 
+
+select * from cidade;
+
+select * from estado;
+
+select * from filial;
+
+select * from genero;
+
+select * from marca;
+
+select * from modelo;
+
+select * from pacote;
+
+select * from pacote_servico;
+
+select * from pessoa;
+
+select * from reserva;
+
+select * from  reserva_servico;
+
+select * from servico;
+
+
+
+
