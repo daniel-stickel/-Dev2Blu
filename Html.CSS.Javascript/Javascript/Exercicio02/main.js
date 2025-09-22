cc960de28dd8fc3f5554499055bc8ad8ca286e17
Daniel Stickel <danielstickel21@gmail.com>
2025-09-22 17:36:22 -0300

    Praticando logica javascript

--- Html.CSS.Javascript/Javascript/Exercicio02/main.js
+++ Html.CSS.Javascript/Javascript/Exercicio02/main.js
@@ -1,3 +1,7 @@
+const btnlistaNomes = document.getElementById('btnlistaNomes')
+const btnAdivinhaNumero = document.getElementById('btnAdivinhaNumero');
+const btnFatorial = document.getElementById('btnFatorial');
+
 /*Instrução If*/
 
 function instrIf01() {
@@ -92,7 +96,7 @@ function arrayOrdemIversa() {
     let nomes = [];
     let nomesInversos;
 
-    for(let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i++) {
         nomes[i] = prompt("Digite um nome:");
     }
 
@@ -101,5 +105,61 @@ function arrayOrdemIversa() {
     /*for(let i = nomes.length -1; i >=0; i--) {
         alert(nomes[i]);
     }*/
-   
+
 }
+
+/* LIstas */  /* Forma certa de chamar um método uma function em javascrpt*/
+
+/* Lista de nomes */
+
+const listaNomes = function () {
+    let nomes = [];
+    alert("Digite quantos nomes desejar ou deixe em branco aperte ENTER")
+
+    for (;;) {
+        let entrada = prompt("Digite o nome para adicionar a lista");
+
+        if(entrada === null || entrada.trim() === ""){
+            break
+        }
+        nomes.push(entrada.trim());
+    }
+
+    let listaFinal =[];
+    for( let nome of nomes) {
+        listaFinal += `\n - ${nome}`; 
+    }
+    alert(listaFinal);
+}
+
+btnlistaNomes.addEventListener('click', listaNomes);
+
+
+/* Adivinhando números da lista */
+
+const advinhaNumeros = function () {
+    const numeros = [12, 21, 36, 37, 55, 63, 73, 82, 89, 96];
+    let entrada = parseInt(prompt("Digite um número de 1 a 100 para ver ser você é bom em adivinhar meus números secretos"));
+
+    if (numeros.includes(entrada)) {
+        alert(`Você é um cara de sorte, o número ${entrada} faz partes dos meus números`);
+    } else {
+        alert('Seu número não é compativel com o meu, você não tem sorte');
+    }
+}
+
+btnAdivinhaNumero.addEventListener('click', advinhaNumeros);
+
+/* Instrução For */
+
+const fatorial = function () {
+    let entrada = parseInt(prompt("Digite um número inteiro para obter o fatorial:"));
+    let resultado = 1;
+
+    for (let i = entrada; i > 1; i--) {
+        resultado = resultado * i;
+    }
+    alert(`O fatorial de ${entrada} é ${resultado}.`);
+}
+
+btnFatorial.addEventListener('click', fatorial);
