93d2a28605c089f3d27c863663a9f07fa08c0bf0
Daniel Stickel <71678898+daniel-stickel@users.noreply.github.com>
2025-09-24 21:54:04 -0300

    atualizando javascript

--- Html.CSS.Javascript/Javascript/Forma certa de chamar no main.js/main.js
+++ Html.CSS.Javascript/Javascript/Forma certa de chamar no main.js/main.js
@@ -1,7 +1,46 @@
-const btnpadrao = document.getElementById('btnpadrao');
+//const btnExemplo01 = document.getElementById('btnExemplo01');
+const btnIncrementar = document.getElementById('btnIncrementar');
+const btnDecrementar = document.getElementById('btnDecrementar');
+const divContadores = document.getElementById('divContadores');
 
-const clickBotao = function () {
+const clickBotaoExemplo01 = function () {
+    alert('Bolinha!')
+}
+
+const clickBotaoExemplo02 = () => {
     alert('Clicou!')
 }
 
-btnpadrao.addEventListener('click', clickBotao);
+const clickBtnIncrementar = () => {
+    let contadores = document.getElementsByClassName('contador');
+    let contador = contadores[contadores.length - 1];
+
+    if (contador.innerText == 10) {
+        adicionarContador();
+    } else {
+        contador.innerText++;
+    }
+}
+
+const clickBtnDecrementar = () => {
+    let contadores = document.getElementsByClassName('contador');
+    let contador = contadores[contadores.length - 1];
+
+    contador.innerText--;
+
+    if (contador.innerText % 10 === 0 && contador.innerText != 0) {
+        divContadores.removeChild(contador);
+    }
+}
+
+const adicionarContador = () => {
+    let contadorBase = document.getElementsByClassName('contador')[0];
+    let contadorNovo = contadorBase.cloneNode();
+
+    contadorNovo.innerText = 1;
+    divContadores.appendChild(contadorNovo);
+}
+
+//btnExemplo01.addEventListener('click', clickBotaoExemplo01)
+btnIncrementar.addEventListener('click', clickBtnIncrementar)
+btnDecrementar.addEventListener('click', clickBtnDecrementar)
