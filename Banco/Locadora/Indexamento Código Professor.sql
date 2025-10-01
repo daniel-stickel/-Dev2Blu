5018bfa77df2cc8906a9058fbca5b76092c87e43
Daniel Stickel <71678898+daniel-stickel@users.noreply.github.com>
2025-09-30 21:49:11 -0300

    finalizando indexadores

--- Banco/Locadora/Indexamento Código Professor.sql
+++ Banco/Locadora/Indexamento Código Professor.sql
@@ -1,49 +1,3 @@
-use locadoraminha;
-
-drop index nome on estado; # para apagar um Index
-
-select * from cidade where nome = "Blumenau";
-
-create index cidade_nome_idx on cidade (nome); # comando para criar um index // create index nomeDaTabela_NomeDaColuna_idx on NomeDaTabela (NomeDoCampo);
-# index é usado quando a muitos dados em uma tabela 
-
-drop index cidade_nome_IDX on cidade;
-
-create index FK_cidade_Cidade_Id_IDX on estado (Id); # 
-create index cidade_nome_idx on cidade (nome);
-
-
-create index carro_nome_IDX on carro (nome);
-
-# Consulta:
-select * from cidade where nome = "Blumenau";
-#Rows antes do 	Indice = 5570
-
- #Indice:
- create index cidade_Nome_IDX on cidade (Nome);
-# Row apos o Indice = 1
-
-
-#Consulta
-explain
-select * from pessoa where Cidade_Id;
-# Rows antes do índice = 201
-
-#Índice
-create index pessoa_Cidade_Id_IDX on pessoa (Cidade_Id);
-#Rows depois do índice = 1
-
-#Consulta:
-explain
-select * from pessoa where Nome;
-# Rows antes do índice = 201
-
-#Índice
-create index pessoa_nome_IDX on pessoa (nome);
-# Rows depois do índice = 1
-
-drop index pessoa_Cidade_Id_IDX on pessoa;
-
 #Consulta
 explain
 select C.Nome as cidade,
@@ -99,10 +53,8 @@ select E.Nome as Estado, count(P.Id) as QuantidadeClienteEstado
     #Rows depois do índice = 28
     # Ganho 714%
     
-    
-    
-    
-#Consulta
+           
+    #Consulta
 explain    
 select C.Nome as Cidade, E.Nome as estados, count(P.Id) as QuantidadeClientes
     from pessoa P 
@@ -146,4 +98,3 @@ select G.Nome as Genero, sum(R.Valor_Total) as ValorTotalReservas, round(avg(R.V
     create index reserva_Pessoa_Id on reserva (Pessoa_Id);
     #Rows depois do índice = 1
     #Ganho de 10000%
-    
