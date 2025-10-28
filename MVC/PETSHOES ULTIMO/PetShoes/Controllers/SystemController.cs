d51e098ca98eb14da6e3ceeb9c18a3354aa935bb
Daniel Stickel <71678898+daniel-stickel@users.noreply.github.com>
2025-10-27 21:46:03 -0300

    adicionando insert ao projeto mvc

---
+++ MVC/PETSHOES ULTIMO/PetShoes/Controllers/SystemController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetShoes.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SystemController : Controller
+    {
+        [HttpGet("")]
+        public bool ValidarConexao()
+        {
+            return PetShoes.Database.Connection.ValidarConexaoBanco();
+        }
+    }
+}
