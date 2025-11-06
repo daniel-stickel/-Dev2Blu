620e7ca18e2d0e7c4a8b7746562fa1fe6c52f9e8
Daniel Stickel <71678898+daniel-stickel@users.noreply.github.com>
2025-11-05 21:51:12 -0300

    Adicionando view

---
+++ MVC/petshoes.mvc/Petshoes.MVC/Models/Pessoas/PessoaResponceModel.cs
@@ -0,0 +1,9 @@
+﻿using Petshoes.MVC.Models.Pessoas;
+
+namespace Petshoes.MVC.Models.Pessoas
+{
+    public class PessoaResponceModel
+    {
+        public IEnumerable<PessoasResponseModel> Pessoas { get; set; }
+    }
+}
